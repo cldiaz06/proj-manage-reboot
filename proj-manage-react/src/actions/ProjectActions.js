d94cbb90c445b1cef7b3ce58fcf1ab128760cc65
cldiaz06 <cldiaz06@gmail.com>
2020-01-23 15:21:52 -0600

    Added delete function and proxy for java server

--- proj-manage-react/src/actions/ProjectActions.js
+++ proj-manage-react/src/actions/ProjectActions.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import {GET_ERRORs, GET_PROJECTS, GET_PROJECT} from './Types';
+import {GET_ERRORs, GET_PROJECTS, GET_PROJECT, DEL_PROJECT} from './Types';
 
 
 export const createProject = (project, history) => async dispatch =>{
     try {
         const res = await axios.post
-            ("http://localhost:8081/api/project", project);
+            ("/api/project", project);
             history.push("/dashboard") ;
             dispatch({
                 type:GET_ERRORs,
@@ -21,7 +21,7 @@ export const createProject = (project, history) => async dispatch =>{
 };
 
 export const getProjects = () => async dispatch => {
-    const res = await axios.get("http://localhost:8081/api/project/all");
+    const res = await axios.get("/api/project/all");
     dispatch({
         type: GET_PROJECTS,
         payload: res.data
@@ -30,7 +30,7 @@ export const getProjects = () => async dispatch => {
 
 export const getProject = (id, history) => async dispatch => {
     try {
-        const res = await axios.get(`http://localhost:8081/api/project/${id}`)
+        const res = await axios.get(`/api/project/${id}`)
         dispatch({
             type: GET_PROJECT,
             payload: res.data
@@ -40,3 +40,14 @@ export const getProject = (id, history) => async dispatch => {
     }
 
 };
+
+export const delProject = (id) => async dispatch =>{
+    if(window.confirm("Are you sure you want to delete this project?")){
+        await axios.delete(`/api/project/${id}`)
+        dispatch({
+            type:DEL_PROJECT,
+            payload: id
+        })
+    }
+}
+
--- proj-manage-react/src/actions/Types.js
+++ proj-manage-react/src/actions/Types.js
@@ -1,5 +1,4 @@
 export const GET_ERRORs = "GET_ERRORS";
-
 export const GET_PROJECTS = "GET_PROJECTS";
-
 export const GET_PROJECT = "GET_PROJECT";
+export const DEL_PROJECT = "DEL_PROJECT";
